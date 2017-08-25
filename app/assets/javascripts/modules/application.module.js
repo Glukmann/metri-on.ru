9b4bfde35d830c3fd1dbaea6700a025f404b9b6f
Alexey Tataurov <zealav@gmail.com>
2017-08-25 14:13:47 +0500

    fix js errors

--- app/assets/javascripts/modules/application.module.js
+++ app/assets/javascripts/modules/application.module.js
@@ -1,20 +1,48 @@
-// Модуль приложения, инициализация глобальных переменных и методов
-// app.modules.applicationModule = (function(self) {
-//   var
-//     _addEvents = function() {
-//       $doc
-//         .on('click', '.selector', function(e) {
-//           e.preventDefault();
-//         });
-//     },
-//
-//     _init = function() {
-//
-//     };
-//
-//   self.load = function() {
-//      _init();
-//      _addEvents();
-//   };
-//   return self;
-// }(app.modules.applicationModule || {}));
+app.modules.applicationModule = (function(self) {
+  var
+    _addEvents = function() {
+      $doc
+        .on('click', '.selector', function(e) {
+          e.preventDefault();
+        });
+
+        $(".scroll").click(function(event){
+          var $this = $(this),
+            offset = $($(this).attr('href')).offset();
+
+          if (!offset) { return false; }
+
+          event.preventDefault();
+
+          $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+          }, 1000);
+        });
+    },
+
+    _init = function() {
+      var pull 		= $('#pull');
+      menu 		= $('nav ul');
+      menuHeight	= menu.height();
+
+      $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+      });
+
+      $(window).resize(function(){
+        var w = $(window).width();
+        if(w > 320 && menu.is(':hidden')) {
+          menu.removeAttr('style');
+        }
+      });
+
+      $().UItoTop();
+    };
+
+  self.load = function() {
+     _init();
+     _addEvents();
+  };
+  return self;
+}(app.modules.applicationModule || {}));
