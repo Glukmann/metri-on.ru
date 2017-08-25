353224fc7f8a7a4d419f08e3f95574a84390baae
Alexey Tataurov <zealav@gmail.com>
2017-08-25 09:55:06 +0500

    add not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from ActionController::RoutingError, with: -> { not_found }
+    rescue_from ActiveRecord::RecordNotFound, with: -> { not_found }
+  end
+
+  def not_found
+    render file: 'public/404.html', status: :not_found, layout: false
+  end
 end
