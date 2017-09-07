e37ce7cbf99093206318f9615e7a60ce269bdc54
andrey.lipanov <Pass@word1>
2017-09-07 14:38:37 +0500

    0.8 alfa catalog

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -2,17 +2,17 @@ class Catalog < ActiveRecord::Base
   require 'csv'
 
   def self.import(file)
-    CSV.foreach(file.path, headers: true, col_sep: ",") do |row|
+    CSV.foreach(file.path, headers: true, col_sep: ";") do |row|
 
       product_hash = row.to_hash
-      product = Catalog.where(odinassid: product_hash["odinassid"])
+      product = Catalog.where(odinassid: product_hash['odinassid'])
 
       if product.count == 1
-        product.first.update_attributes(product_hash)
+        product.first.update_attributes(odinassid: product_hash['odinassid'], odinassname: product_hash['odinassname'], article: product_hash['article'], col: product_hash['col'])
       else
-       # Catalog.create!(product_hash)
-        p = Catalog.new(odinassid: product_hash['odinassid'])
-        p.save
+       Catalog.create!(odinassid: product_hash['odinassid'], odinassname: product_hash['odinassname'], article: product_hash['article'], col: product_hash['col'])
+       #  p = Catalog.new(odinassid: product_hash['odinassid'])
+       #  p.save
       end
     end # end CSV.foreach
   end # end self.import(file)
