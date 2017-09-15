00b8cd10690b764834c75d51cebbdba6ea4b9633
Alexey Tataurov <zealav@gmail.com>
2017-09-15 11:36:31 +0500

    remove not null from catalogs migration

--- db/migrate/20170906125839_add_newcolums_to_catalogs.rb
+++ db/migrate/20170906125839_add_newcolums_to_catalogs.rb
@@ -1,6 +1,6 @@
 class AddNewcolumsToCatalogs < ActiveRecord::Migration
   def change
-    add_column :catalogs, :odinassid, :string, null: false, index: true
-    add_column :catalogs, :odinassname, :string, null: false, index: true
+    add_column :catalogs, :odinassid, :string, index: true
+    add_column :catalogs, :odinassname, :string, index: true
   end
 end
